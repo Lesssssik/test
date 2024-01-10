3c3a2a0a050f827bbdfdb7f2988ff5aa28308ea7
Olesya <olesyap19@gmail.com>
2024-01-10 19:42:46 +0300

    the second commit

---
+++ src/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+    public double summa(double a, double b){
+
+    return a+b;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,8 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.summa(1, 2));
 
     }
 }
